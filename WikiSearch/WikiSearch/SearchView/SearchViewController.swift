d9c0aae5facb58995f447266d07f7cf3e47373c5
Agam Mahajan <agam.mahajan@practo.com>
2018-09-01 01:38:03 +0530

    fetch data from api

--- WikiSearch/WikiSearch/SearchView/SearchViewController.swift
+++ WikiSearch/WikiSearch/SearchView/SearchViewController.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2018 Agam Mahajan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+
+	@IBOutlet weak var searchBar: UISearchBar!
+	@IBOutlet weak var tableView: UITableView!
+
+	var viewModel: SearchViewModel!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		viewModel = SearchViewModel.initViewModel()
+	}
+
+}
+
+extension SearchViewController: UISearchBarDelegate {
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		guard let searchText = searchBar.text else {return}
+		viewModel.search(text: searchText)
+	}
+}
--- WikiSearch/WikiSearch/SearchView/SearchViewModel.swift
+++ WikiSearch/WikiSearch/SearchView/SearchViewModel.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class SearchViewModel {
+
+	let dataSource: [Page] = []
+
+	static func initViewModel() -> SearchViewModel {
+		let viewModel = SearchViewModel()
+		return viewModel
+	}
+
+	func search(text: String) {
+		let url = "https://en.wikipedia.org//w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&wbptterms=description&gpslimit=10&gpssearch=" + text
+		let manager: NetworkManager = NetworkManager()
+		manager.get(urlString: url, params: nil, headers: nil, body: nil, success: { (response, responsData) in
+			print(responsData)
+		}) { (response, responseData, error) in
+			print(response)
+		}
+	}
+
+}
